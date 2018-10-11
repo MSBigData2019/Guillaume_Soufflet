c887317b7e4f3f8b395ba365cc9ad4a7567e6825
Guillaume <guillaume.soufflet@telecom-paristech.fr>
2018-10-11 21:58:26 +0200

    changed dividend yield to make it explicit and use of dictionnary to collect elementary piece of data

--- Lesson2/exo_dom_lesson_2.py.py
+++ Lesson2/exo_dom_lesson_2.py.py
@@ -32,12 +32,21 @@ def get_data(company):
     res = requests.get(url+company)
     soup = _handle_request_result_and_build_soup(res)
     tags_td = soup("td")
-    data = {}
-    data['Current Stock price'] = _find_current_stock_price(soup)
-    data['Change rate'] = _find_current_change_rate(soup)
-    data['Q4 2018 sales'] = _find_Q4_sales(tags_td)
-    data['Shares owned by Institutional Holders'] = _shares_owned_by_inst(tags_td)
-    data['Dividend Yield'] = _dividend_yield(tags_td)
+    d1 = _find_current_stock_price(soup)
+    d2 = _find_current_change_rate(soup)
+    d3 = _find_Q4_sales(tags_td)
+    d4 = _shares_owned_by_inst(tags_td)
+    d5 = _dividend_yield(tags_td)
+    data = dict(d1, **d2)
+    data.update(d3)
+    data.update(d4)
+    data.update(d5)
+#    data = dict(d1, d2, d3, d4, d5)
+#    data['Current Stock price'] = _find_current_stock_price(soup)
+#    data['Change rate'] = _find_current_change_rate(soup)
+#    data['Q4 2018 sales'] = _find_Q4_sales(tags_td)
+#    data['Shares owned by Institutional Holders'] = _shares_owned_by_inst(tags_td)
+#    data['Dividend Yield'] = _dividend_yield(tags_td)
     return data
 
 #%%
@@ -52,46 +61,52 @@ def _clean_string(string): # remove \t, \n, (, ), whitespace
 
     #%%
 def _find_current_stock_price(soup):   
+    d1 = {}
     # Searching stock price in soup
     tag_stock = soup.find_all("span", style=True)[0].text
     # tag_stock = soup.find_all('span')[5] # alternative searching way
-    stock = _clean_string(tag_stock)
-    return stock
+    d1['Current Stock price']= _clean_string(tag_stock)
+    return d1
 
 #%%
 def _find_current_change_rate(soup):    
+    d2 ={}
     tag_stock_var = soup.find_all("span", class_= "valueContentPercent")[0]
     stock_var_raw = tag_stock_var.findAll('span')[0].contents[0].string
-    stock_var = _clean_string(stock_var_raw)  
-    return stock_var
+    d2['Change rate'] = _clean_string(stock_var_raw)  
+    return d2
     
 #%%
 def _find_Q4_sales(tags_td):     # Find quarter sales (mean) at end 2018
+    d3={}
     Q4_list = []
     for i in range(len(tags_td)):
         if tags_td[i].text == 'Quarter Ending\xa0Dec-18':
             Q4_list.append(tags_td[i+2].text)
-    return Q4_list[0] # 2 values found (sales & earnings): only sales is kept
+    d3['Q4 2018 sales'] = Q4_list[0]
+    return d3 # 2 values found (sales & earnings): only sales is kept
     # TODO: replace string with comma for thousand by a number 
 
 #%%
 
 def _shares_owned_by_inst(tags_td):
+    d4 ={}
     for i in range(len(tags_td)):
         if tags_td[i].text == '% Shares Owned:':
-            share_list = tags_td[i+1].text
-    return share_list
+            d4['Shares owned by Institutional Holders'] = tags_td[i+1].text
+    return d4
 
 #%%
     
 def _dividend_yield(tags_td): # function with list
+    d5 = {}
     yield_list = []
     for i in range(len(tags_td)):
         if tags_td[i].text == 'Dividend Yield':
-            yield_list.append(tags_td[i+1].text)
-            yield_list.append(tags_td[i+2].text)
-            yield_list.append(tags_td[i+3].text)
-    return yield_list
+            d5['Dividend Yield Company'] = tags_td[i+1].text
+            d5['Dividend Yield Industry'] = tags_td[i+2].text
+            d5['Dividend Yield Sector'] = tags_td[i+3].text
+    return d5
         
 #def _dividend_yield(tags_td): # Alternative with dict
 #    yield_dict = {}
@@ -105,19 +120,17 @@ def _dividend_yield(tags_td): # function with list
 #%%
 companies = ['LVMH.PA', 'AIR.PA', 'DANO.PA']
 result = get_key_figures(companies)
-for company in companies:
-    print(result[company])
+print(result)
 
-expected_result = 
-
-class Lesson2Tests(unittest.TestCase):
-    def testGetKeyFigures(self):
-        self.assertEqual(get_key_figures(companies), expected_result)
-        
-
-def main():
-    unittest.main()
-
-if __name__ == '__main__':
-    main()
+#expected_result = 
+#
+#class Lesson2Tests(unittest.TestCase):
+#    def testGetKeyFigures(self):
+#        self.assertEqual(get_key_figures(companies), expected_result)
+#        
+#def main():
+#    unittest.main()
+#
+#if __name__ == '__main__':
+#    main()
     
